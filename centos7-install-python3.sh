29b3245f91da7fe7fbef973f9b2c03c7cdfd8596
JIAHE <jiahe@bigrule.com>
2021-01-19 14:14:07 +0800

    modify yum something for centos7-install-python3.sh

--- centos7-install-python3.sh
+++ centos7-install-python3.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-yum -y groupinstall "Development tools"
-yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
-yum install -y libffi-devel zlib1g-dev
-yum install zlib* -y
+yum -y install gcc make cmake zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel libffi-devel
 
 wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tar.xz
 
@@ -12,11 +9,11 @@ tar -xvJf  Python-3.7.2.tar.xz
 mkdir /usr/local/python3 
 
 cd Python-3.7.2
-./configure --prefix=/usr/local/python3 --enable-optimizations --with-ssl 
+/configure --prefix=/usr/local/python3 --enable-optimizations
 #第一个指定安装的路径,不指定的话,安装过程中可能软件所需要的文件复制到其他不同目录,删除软件很不方便,复制软件也不方便.
 #第二个可以提高python10%-20%代码运行速度.
 #第三个是为了安装pip需要用到ssl,后面报错会有提到.
-make && make install
+make -j4 && make install -j4
 
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3
 ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3
